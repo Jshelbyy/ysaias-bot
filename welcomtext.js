f5189e61a7622c2af405a28773c39444b865fa0a
Jshelbyy <77311737+Jshelbyy@users.noreply.github.com>
2021-04-12 17:08:19 -0300

    Add files via upload

---
+++ welcomtext.js
@@ -0,0 +1,9 @@
+const welcome = (number, groupname) => {
+    return `𝐎𝐥𝐚 @${number}. 𝐒𝐞𝐣𝐚 𝐛𝐞𝐦-𝐯𝐢𝐧𝐝𝐨 𝐚𝐨 𝐠𝐫𝐮𝐩𝐨 ${groupname}`
+}
+exports.welcome = welcome
+
+const bye = (number) => {
+    return `𝗺𝗮𝗰𝗮𝗰𝗼 🐒 𝗳𝗼𝗶 𝗲𝗺𝗯𝗼𝗿𝗮 𝗲 𝗻𝗲𝗺 𝗽𝗮𝗴𝗼𝘂 𝘂𝗺𝗮 𝗰𝗼𝘅𝗶𝗻𝗵𝗮 𝗽𝗿𝗼 𝗴𝗿𝘂𝗽𝗼 😔���`
+}
+exports.bye = bye
